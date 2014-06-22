14860f448d8604582b10bd0ca0b16221529ed680
dq100 <dq100@users.noreply.github.com>
2014-06-22 18:56:22 +0200

    Einbindung von MPD Menü und Now Playing

--- piradio.c
+++ piradio.c
@@ -5,15 +5,65 @@
 #include "helper.h"
 #include "lcdpi.h"
 #include "rotaryencoder.h"
+#include "radio.h"
+
+struct encoder *encoder;
+
 
 void tmp_action(menu_item* item)
 {
 	printInfo(item->text);
 }
 
+void start_action(menu_item* item)
+{
+	printInfo(item->text);
+	start_mpd();
+}
+
+void stop_action(menu_item* item)
+{
+	printInfo(item->text);
+	stop_mpd();
+}
+
+void now_playing(menu_item* item)
+{
+
+	char *title = get_current_songtitle();
+	lcd_line_print(LINE1, title);
+
+	while(1)
+	{
+		//aktuellen Titel ausgeben, wenn er sich geändert hat:
+		if(strcmp(title,get_current_songtitle()) != 0)
+		{
+			title = get_current_songtitle();
+			lcd_line_print(LINE1, title);
+		}
+		direction dir = getDirection(encoder);
+
+		//Lauter
+		if(dir==DIR_LEFT)
+			set_volume(10);
+
+		//Leiser
+		if(dir==DIR_RIGHT)
+			set_volume(-10);
+
+		//Menü aufrufen
+		if(getButtonPressed(encoder)==1)
+		{
+			menu_draw();
+			break;
+		}
+		delay(100);
+
+	}
+}
+
 int main(int argc, char **argv)
 {
-	struct encoder *encoder;
 	int i = 0;
 
 	printInfo("==================================================");
@@ -49,6 +99,7 @@ int main(int argc, char **argv)
 	delay(3000);
 
 	encoder = setupencoder(9, 7, 8);
+	init_mpd();
 
 
 	//Hauptmenü starten
@@ -56,24 +107,24 @@ int main(int argc, char **argv)
 	init_menu();
 	selected_menu.active = 1;
 
-	menu_item_add_subitem(item_main_menu, create_menu_item("Hallo", tmp_action, 0));
-	menu_item_add_subitem(item_main_menu, create_menu_item("Hallo2", tmp_action, 0));
-	menu_item_add_subitem(item_main_menu, create_menu_item("Hallo3", tmp_action, 0));
-	menu_item_add_subitem(item_main_menu, create_menu_item("Hallo4", tmp_action, 0));
-	menu_item_add_subitem(item_main_menu, create_menu_item("Hallo5", tmp_action, 0));
+	menu_item_add_subitem(item_main_menu, create_menu_item("Start", start_action, 0));
+	menu_item_add_subitem(item_main_menu, create_menu_item("Stop", stop_action, 0));
+	menu_item_add_subitem(item_main_menu, create_menu_item("Zurück", now_playing, 0));
+	//menu_item_add_subitem(item_main_menu, create_menu_item("Hallo4", tmp_action, 0));
+	//menu_item_add_subitem(item_main_menu, create_menu_item("Hallo5", tmp_action, 0));
 
 	printDebug("Zeichne Menue...");
 	menu_draw();
 
 	printDebug("Starte Hauptprogramm");
+
+
+
 	while(1)
 	{
 		//Hauptprogramm
 
-
 		//checkDirection
-		//checkButtons
-
 		direction dir = getDirection(encoder);
 
 		if(dir==DIR_LEFT)
@@ -81,6 +132,7 @@ int main(int argc, char **argv)
 		if(dir==DIR_RIGHT)
 			menu_inc();
 
+		//checkButton
 		if(getButtonPressed(encoder)==1)
 		{
 			printf("Button pressed! \n");
