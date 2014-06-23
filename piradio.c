ce7b30042049cc334a6ae5186f75484435fbdb05
dq100 <dq100@users.noreply.github.com>
2014-06-23 22:06:16 +0200

    Uhrzeit eingebaut, unötiges gelöscht

--- piradio.c
+++ piradio.c
@@ -10,23 +10,47 @@
 #include <time.h>
 
 struct encoder *encoder;
+int radiostation = 0;
+
+char * getTime()
+{
+	time_t current_time;
+	struct tm * time_info;
+	char *cur_time = 0;
+	char timeString[20];  // space for "HH:MM\0"
+
+	time(&current_time);
+	time_info = localtime(&current_time);
 
 
-void tmp_action(menu_item* item)
+	strftime(timeString, sizeof(timeString), "%H:%M", time_info);
+
+	cur_time = (char*)calloc(strlen(timeString), sizeof(char));
+	strcpy(cur_time, timeString);
+
+	return cur_time;
+}
+
+
+
+void quit_action(menu_item* item)
 {
 	printInfo(item->text);
+	system("init 0");
 }
 
 void start_action(menu_item* item)
 {
 	printInfo(item->text);
 	start_mpd();
+	//now_playing();
 }
 
 void change_action(menu_item* item)
 {
 	printInfo(item->text);
-	change_radiostation(1);
+	radiostation=1;
+	change_radiostation(radiostation);
 }
 
 void stop_action(menu_item* item)
@@ -42,29 +66,45 @@ void now_playing(menu_item* item)
 	lcd_display_clear();
 
 
+	//lcd_line_print(LINE1, rs[radiostation]->RadioName);
+
 	char *title = get_current_songtitle();
+	lcd_line_print(LINE2, title);
 
-	lcd_line_print(LINE1, title);
 
-
+	char *timenow = getTime();
+	printf("%s\n",timenow);
+	lcd_line_print(LINE4, timenow);
 	printf("%s\n", title);
-
 
 
 
 
 	while(1)
 	{
-
 		//aktuellen Titel ausgeben, wenn er sich geändert hat:
-		if((strcmp(title,get_current_songtitle()) != 0) )
+		if((strcmp(title,get_current_songtitle()) != 0))
 		{
 			free(title);
-			lcd_display_clear();
+			//lcd_display_clear();
+			lcd_line_clear(LINE2);
 			title = get_current_songtitle();
 			printf("%s\n", title);
-			lcd_line_print(LINE1, title);
+			lcd_line_print(LINE2, title);
 		}
+
+		//aktuelle Uhrzeit ausgeben, wenn sie sich geändert hat
+		if((strcmp(timenow,getTime()) != 0))
+		{
+			free(timenow);
+			lcd_line_clear(LINE4);
+			timenow = getTime();
+			printf("%s\n", timenow);
+			lcd_line_print(LINE4, timenow);
+		}
+
+
+
 		direction dir = getDirection(encoder);
 
 		//Leiser
@@ -85,6 +125,7 @@ void now_playing(menu_item* item)
 
 	}
 	free(title);
+	free(timenow);
 }
 
 int main(int argc, char **argv)
@@ -134,9 +175,9 @@ int main(int argc, char **argv)
 
 	menu_item_add_subitem(item_main_menu, create_menu_item("Start", start_action, 0));
 	menu_item_add_subitem(item_main_menu, create_menu_item("Stop", stop_action, 0));
-	menu_item_add_subitem(item_main_menu, create_menu_item("Zurueck", now_playing, 0));
 	menu_item_add_subitem(item_main_menu, create_menu_item("Sender wechseln", change_action, 0));
-	//menu_item_add_subitem(item_main_menu, create_menu_item("Hallo5", tmp_action, 0));
+	menu_item_add_subitem(item_main_menu, create_menu_item("Zurueck", now_playing, 0));
+	menu_item_add_subitem(item_main_menu, create_menu_item("Beenden", quit_action, 0));
 
 	printDebug("Zeichne Menue...");
 	menu_draw();
